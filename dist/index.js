f3ad5c997f09b42b7c81554bd81042f51dbda247
Nick Lima <nlima446@gmail.com>
2023-06-06 21:12:10 -0700

    forgot to export correct one

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ import Scatterplot from './Scatterplot.js'; // const ScatterplotLegend = require
 import ScatterplotLegend from './ScatterplotLegend.js'; // const Legend = require('./Legend.js');
 
 import Legend from './Legend.js';
-import Table from './Table.js';
+import Table from './ActiveTable.js';
 import DropdownFilter from './DropdownFilter.js'; // const Table = require('./Table.js');
 
 export { Scatterplot, ScatterplotLegend, Legend, Table, DropdownFilter };
